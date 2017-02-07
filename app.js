ca2a584689881fb1150ac59e62549f7a5cf9f6b6
newaz <newaz.sharif@hotmail.com>
2017-02-08 00:18:15 +0600

    p1v8 updated

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-require('./instantHello');
-var goodBye = require('./talk/goodBye');
-var talk = require('./talk');
-var question = require('./talk/question')
+(function()
+{
+	var express = require('express');
+	var app = express();
+	var path  = require('path');
 
+	app.set('port',3000);
+	app.get('/',function(req,res)
+	{
+		console.log("got it man");
+		res.sendFile(path.join(__dirname,'./public/index.html'))
+	})
 
+	app.get('/json',function(req,res)
+	{
+		console.log("got it man");
+		res.json({'name' : 'newaz'}).status(404);
+	})
+	app.get('/file',function(req,res)
+	{
+		res.sendFile(path.join(__dirname,'app.js'));
+	})
 
-goodBye();
-talk.intro();
-talk.hello('Shakil'); 
-var result = question.ask();
-console.log(result);
+	app.listen(app.get('port'),function()
+		{
+			console.log("hello from port "+ app.get('port'));
+		});
+	console.log('test')
+})()
